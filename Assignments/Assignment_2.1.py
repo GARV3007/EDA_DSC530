3700e660953428ae86c2da37a150783433092692
Gourav Verma <48499611+GARV3007@users.noreply.github.com>
2020-01-26 12:23:00 -0600

    Add files via upload

---
+++ Assignments/Assignment_2.1.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+# File : Assignment 2.1.py
+# Name : Gourav Verma
+# Date : 8/12/2019
+# Course : DSC-530 - Exploratory Data Analysis
+# Description  : Week-2 : Preparing for Exploratory Data Analysis
+
+@author: Gourav
+"""
+
+""" Display the text """
+print("Hello World! I wonder why that is always the default coding text to start with")
+
+a = 5
+b = 9
+text1 = "Santa Claus has the right idea."
+text2 = "Visit people only once a year."
+
+""" Add two numbers """
+c = a + b
+print("Sum of a and b is : ", c)
+
+""" Substract a number from another """
+c = a - b
+print("Substraction is : ", c)
+
+""" Multiply two numbers """
+c = a * b
+print("Multiplication is : ", c)
+
+""" Divide between two number """
+c = a/b
+print("Division is : ", c)
+
+""" Concatenat two strings"""
+ctext = text1 + text2
+print("Concatenated String is : ", ctext) 
+
+""" Create list """
+list = [1, "Brave", "Cell", 3.4]
+print("list is : ", list)
+
+""" Add item to the list """
+list.append(9)
+print("New list is : ", list)
+
+""" Create a Tuple """
+Tup = (1, "Banana", 3, "Grapes")
+print("Tuple is : ", Tup)
